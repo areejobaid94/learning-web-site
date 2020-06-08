811505984ffda29d287a4f84f592fdb8fa481aac
Areej Obaid <areej.obaid@yahoo.com>
2020-06-09 00:26:03 +0300

    adding DB

--- LearningBackEnd/LearningBackEnd/Startup.cs
+++ LearningBackEnd/LearningBackEnd/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LearningBackEnd.models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,8 @@ namespace LearningBackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<DBContext>
+            services.AddDbContext<DBContext>(options =>
+            options.UseSqlServer())
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
